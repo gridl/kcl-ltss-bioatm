afeb6e594e9004fd9ef4ef15dab90c01a5cf30ab
Daniel Fisher <danielfisher0@gmail.com>
2018-04-24 09:27:39 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='A unet based classifier for extracting smoke plumes from EO images',
+    author='dnfisher',
+    license='MIT',
+)
